1e94ffa004aabffe01574a6f184ae75420003e6e
mandeep kumar <mandeepkumar4567852@gmail.com>
2024-09-10 12:51:12 +0530

    first commit

---
+++ OneDrive/Desktop/cpp/binary.cpp
@@ -0,0 +1,14 @@
+//binary
+//*type casting-->to conver one data type into another data type ,either origrammer itself or by compiler
+//(1)explict-->programmer
+//(2)implict-->compiler (itself)automatic
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=12;
+	int b=5;
+	cout<<(float)a/b<<endl;//explict
+	cout<<a/7.0<<endl;
+	cout<<'a'+1<<endl;
+}
---
+++ OneDrive/Desktop/cpp/bitwise.cpp
@@ -0,0 +1,22 @@
+//bitwise operator; calculation based on binary
+//tyoes:-
+//1.& (And) operator
+//2.| (or) operator
+//3. ^ (xor) operator
+//4. ~ (negation) opeartor
+//5. >> (right shift) operator
+//6. << (left shift) opeartor
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+//    a=12&19; (And operator)
+//    a=23 | 51; (Or operator)
+//    cout<< !a; (Not operator)
+//    a=13 ^ 9;// (XOR operator)
+//	  cout<<~a; //formula -(a+1)
+     a=40<<2;
+     cout<<a;
+}
+
