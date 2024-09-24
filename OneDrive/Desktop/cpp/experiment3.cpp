7d5c8b2c0439b9aa20d097476395a75eacfb0a41
mandeep kumar <mandeepkumar4567852@gmail.com>
2024-09-24 13:29:15 +0530

    add while loop

--- OneDrive/Desktop/cpp/experiment3.cpp
+++ OneDrive/Desktop/cpp/experiment3.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main(){
 	
 //	pattern1
-	for(int i=1;i<=5;i++){
-		for(int j=1;j<=i;j++){
-			cout<<j<<" ";
-		}
-		cout<<endl;
-	}
+//	for(int i=1;i<=5;i++){
+//		for(int j=1;j<=i;j++){
+//			cout<<j<<" ";
+//		}
+//		cout<<endl;
+//	}
 	
 //	pattern2
 //	for(int i=1;i<=5;i++){
@@ -102,22 +102,22 @@ int main(){
 
 
 
-//int star=1;
-//for(int i=5;i>=1;i--){
-//	for(int j=i;j>=1;j--){
-//		cout<<" ";
-//	}
-//	for(int k=1;k<=star;k++){
-//		if(k%2==0){
-//			cout<<" * ";
-//		}
-//		else{
-//		   cout<<" ";
-//		}
-//	}
-//	star=star+2;
-//	cout<<endl;
-//}
+int star=1;
+for(int i=5;i>=1;i--){
+	for(int j=i;j>=1;j--){
+		cout<<" ";
+	}
+	for(int k=1;k<=star;k++){
+		if(k%2==0){
+			cout<<" * ";
+		}
+		else{
+		   cout<<" ";
+		}
+	}
+	star=star+2;
+	cout<<endl;
+}
 
 	
 //char ch='a';
--- OneDrive/Desktop/cpp/forloop.cpp
+++ OneDrive/Desktop/cpp/forloop.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int i=2,j=3;
+//    int i=2,j=3;
 //    for(i+1;i<=10; i++){
 //    	cout<<i<<"\t";
 //	}
@@ -20,8 +20,155 @@ int main(){
 //    for(1;1;1){
 //    	cout<<"bye"<<"\t";
 //	}
-    for(;5;){
-    	cout<<i<<"\t";
-	}
+//    for(;5;){
+//    	cout<<i<<"\t";
+//	}
+//    for(;0;){
+//    	cout<<"bye"<<"\t";
+//	}
+//	cout<<"\ndone";
+
+
+
+//   for(int i=0; i<5; i++){
+//   	for(int j=0; j<6; j++){
+//   		cout<<" *"<<" ";
+//	   }
+//	   cout<<endl;
+//   }
+
+   
+//     for(int i=1;i<=5;i++){
+//		for(int j=1;j<=i;j++){
+//			cout<<"*"<<" ";
+//		}
+//		cout<<endl;
+//	}
+ 
+//   for(int i=1;i<=5;i++){
+//		for(int j=1;j<=i;j++){
+//			cout<<j<<" ";
+//		}
+//		cout<<endl;
+//	}
+
+//    int n=1;
+//     for(int i=1;i<=5;i++){
+//		for(int j=1;j<=i;j++){
+//			cout<<n<<" ";
+//			n++;
+//		}
+//		cout<<endl;
+//	}
+
+
+   
+//     for(int i=1;i<=5;i++){
+//		for(int j=1;j<=i;j++){
+//			cout<<j<<" ";
+//			
+//		}
+//		cout<<endl;
+//	}
+
+
+//     int n=1;
+//     for(int i=1;i<=5;i++){
+//		for(int j=1;j<=i; j++){
+//			cout<<j*j<<" ";
+////			n=n+3;
+//		}
+//		cout<<endl;
+//	}
+
+
+    
+//     for(int i=1;i<=5;i++){
+//		for(int j=1;j<=i;j++){
+//			cout<<j<<" ";
+//			
+//		}
+//		cout<<endl;
+//	}
+    
+    
+//   for(char i='A';i<='Z';i++){
+//		for(char j='A';j<=i;j++){
+//			cout<<j<<" ";
+//			
+//		}
+//		cout<<endl;
+//	} 
+ 
+//    int n=65;
+//    for(int i=1; i<=3;i++){
+//		for(char j=1 ;j<=i;j++){
+//			cout<<char(n)<<" \t";
+//			n++;
+//			
+//		}
+//		cout<<endl;
+//	}
+
+//    for(int i = 1; i <=5; i++){
+//    	for(int j =1; j<=i; j++){
+//    		cout<<"*"<<" ";
+//		}
+//		cout<<endl;
+//	}
+	
+//	for(int k=5; k>=1; k--){
+//			for(int l=1; l<=k; l++){
+//				cout<<"*"<<" ";
+//			}
+//			cout<<endl;
+//		}
+
+//    for(int i=1; i <=5; i++){
+//    	for(int j=1; j<=i; j++){
+//    		if((i+j)%2==0){
+//    			cout<<"0";
+//			}
+//			else{
+//				cout<<"1";
+//			}
+//		}
+//		cout<<endl;
+//	}
+
+   for(int r=1; r<=3; r++){
+   	for(int s=3; s>r; s--){
+   		cout<<" ";
+	   }
+	   for(int c=1; c<=r; c++){
+	   	cout<<"*";
+	   }
+	   cout<<endl;
+   }
+   
+
+
+    
+    
+
+ 
+ 
+
+
+
+//   for(int i=1; i<=5;i++){
+//   	for(int j=1; j<=8; j++){
+//   		if(i==1 || i==5 || j==1 || j==8){
+//   			cout<<"*";
+//		   }
+//		   else{
+//		   	cout<<" ";
+//		   }
+//	   }
+//	   cout<<endl;
+//   }
+
+
+
 }
 
