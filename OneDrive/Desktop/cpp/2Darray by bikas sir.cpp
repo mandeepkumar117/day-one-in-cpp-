bf0ff6ddd8d882209477ad03483fa0beaaf169c6
mandeep kumar <mandeepkumar4567852@gmail.com>
2024-10-05 12:35:19 +0530

    add 2d array

---
+++ OneDrive/Desktop/cpp/2Darray by bikas sir.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+int main(){
+//    int arr[2][3];
+//    cout<<"enter 6 values\n";
+//    for(int r=0; r<2; r++){
+//    	for(int c=0; c<3; c++){
+//    		cin>>arr[r][c];
+//		}
+//	}
+//	cout<<"\n stored data in 2d\n";
+//    for(int r=0; r<2; r++){
+//    	for(int c=0; c<3; c++){
+//    		cout<<arr[r][c]<<"\t";
+//		}
+//		cout<<"\n";
+//	}
+
+//int arr[3][3];
+//cout<<"enter 9 values\n";
+//for(int r=0; r<3; r++){
+//	for(int c=0; c<3; c++){
+//		cin>>arr[r][c];
+//	}
+//}
+//cout<<"\n store even data in 2d\n";
+//for(int r=0; r<3; r++){
+//	for(int c=0; c<3; c++){
+//		if(arr[r][c] % 2 ==0){
+//			cout<<arr[r][c]<<"\t";
+//			
+//		}
+//		cout<<"\n";
+//	}
+//}
+
+
+
+
+int arr[3][3];
+cout<<"enter 9 values\n";
+for(int r=0; r<3; r++){
+	for(int c=0; c<3; c++){
+		cin>>arr[r][c];
+	}
+}
+cout<<"\n store even data in 2d\n";
+int sum=0;
+for(int r=0; r<3; r++){
+	for(int c=0; c<3; c++){
+		sum=sum+arr[r][c];
+		cout<<"sum of array"<<sum<<"\t";
+	}
+	cout<<"\n";
+}
+}
+
