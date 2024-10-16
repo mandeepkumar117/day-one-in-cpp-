5a9cc2e7ab36415e1a701fa3263d9f59a1f7803e
mandeep kumar <mandeepkumar4567852@gmail.com>
2024-10-16 12:44:40 +0530

    add new file on 16th october

---
+++ OneDrive/Desktop/cpp/function.cpp
@@ -0,0 +1,22 @@
+//function->it use to perform any task
+//1)reusability
+//2)memory saving
+//3)time saving
+//types->
+//1)pre-Define
+//2)user-Define
+
+#include<iostream>
+using namespace std;
+#include<math.h>
+int main(){
+    int a;
+    cout<<"enter any value\n";
+    cin>>a;
+    cout<<sqrt(a);
+    cout<<"\n";
+    cout<<sqrt(64);
+    cout<<"\n";
+    cout<<"power:-"<<pow(3,2);
+}
+
